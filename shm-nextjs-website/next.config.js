649c3cb0ff8e72a37c88cf8b45e4b1be01436e0b
Andres Seivane <tu@correo.com>
2023-06-08 00:23:12 -0300

    automatic update for dev

--- shm-nextjs-website/next.config.js
+++ shm-nextjs-website/next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  webpackDevMiddleware: (config) => {
+    // Solve compiling problem via vagrant
+    config.watchOptions = {
+      poll: 1000,   // Check for changes every second
+      aggregateTimeout: 300,   // delay before rebuilding
+    };
+    return config;
+  }
 }
 
-module.exports = nextConfig
+// module.exports = nextConfig
+module.exports = nextConfig;
