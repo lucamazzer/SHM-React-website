c3d80e46769431e9d36118d8aa31a85193eb6e7e
aseivane <40507382+aseivane@users.noreply.github.com>
2023-07-12 15:43:40 -0300

    env var to define host name (#6)
    Co-authored-by: Andres Seivane <tu@correo.com>

--- shm-nextjs-website/src/api/api.js
+++ shm-nextjs-website/src/api/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 export const DEFAULT_RETRIES = 2;
 export const CONFIG_TIMEOUT = 0;
-export const API_URL = 'http://localhost/api/';
+export const HOST_NAME = process.env.NEXT_PUBLIC_HOST_NAME
+export const API_URL = `http://${HOST_NAME}/api/`;
 
 const apiConfig = {
   baseURL: API_URL,
