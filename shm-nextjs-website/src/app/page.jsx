46d1e4eb73860acd32c1f6ae0ca5e33edda01989
lmazzer <lucamazzer92@gmail.com>
2023-09-03 19:33:46 -0300

    fix: home and graphics screens overflow

--- shm-nextjs-website/src/app/page.jsx
+++ shm-nextjs-website/src/app/page.jsx
@@ -2,12 +2,12 @@ import Image from 'next/image';
 
 export default function HomePage() {
   return (
-    <div className="flex flex-col text-center justify-center items-center bg-primary">
+    <div className="flex h-full flex-col text-center justify-center items-center bg-primary">
       <h1 className="text-5xl	mt-10 text-white">
         SHM - SISTEMA DE MONITOREO DE SALUD ESTRUCTURAL
       </h1>
       <Image
-        className="flex flex-1 mt-10"
+        className="flex overflow-auto mt-10"
         src={require('../assets/fiuba.jpg')}
         alt={'Fiuba :D'}
       />
