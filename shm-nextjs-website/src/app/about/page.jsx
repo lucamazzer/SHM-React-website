bba43982d3e3ff6feb6face5442d3fd3665ce1aa
lmazzer <lucamazzer92@gmail.com>
2023-12-11 00:14:05 -0300

    pdf wip

--- shm-nextjs-website/src/app/about/page.jsx
+++ shm-nextjs-website/src/app/about/page.jsx
@@ -1,7 +1,16 @@
+// import dynamic from 'next/dynamic';
+
+import PDFViewer from '../components/PdfReader';
+
+// const PDFViewer = dynamic(() => import('../components/PdfReader'), {
+//   ssr: false,
+// });
+
 export default function AboutPage() {
   return (
-    <div>
-      <h1 className="text-center text-4xl mt-5 text-white">INSTRUCCIONES</h1>
+    <div className="h-full p-5 bg-gray-300 overflow-auto shrink-0">
+      <h1 className="text-center text-4xl">INSTRUCCIONES</h1>
+      <PDFViewer />
     </div>
   );
 }
